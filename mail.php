f215ae4aea359483f369c26bea1cfa32e30846c8
Mohamed M. Abdelhamid <mhmedmhmod184@gmail.com>
2022-03-20 03:49:07 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -5,13 +5,13 @@ $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
 $to = "mhmedmhmod@gmail.com";
-$subject = $_POST['subject'];
+$subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@github.io.com" . "\r\n" .
+$headers = "From: noreply@yoursite.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
-header("thankyou.html");
+header("Location:thankyou.html");
 ?>
